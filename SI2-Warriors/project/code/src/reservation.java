b6e8ed1fbea37467ad262f0b76ec1decbfa2d2d2
Aravinth <aravinth.shanthir@gmail.com>
2020-12-02 16:18:24 +0100

    Ajout des classes billet, reservation et aeroport

---
+++ SI2-Warriors/project/code/src/reservation.java
@@ -0,0 +1,12 @@
+public class reservation {
+    int numero_reservation;
+    boolean reserved;
+
+    public reservation(int num, boolean reserved)
+    {
+        this.numero_reservation=num;
+        this.reserved=reserved;
+    }
+
+
+}
