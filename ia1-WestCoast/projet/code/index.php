629ab98e67e8e028c1522c9198770ce4999598e3
Florent-Ngt <nflorent7@gmail.com>
2020-12-03 14:02:23 +0100

    Basecode application

---
+++ ia1-WestCoast/projet/code/index.php
@@ -0,0 +1,29 @@
+ <?php
+    //======================================================================
+    // INDEX.PHP       TP - Prog distribuee       GROUPE - WestCoast
+    //======================================================================
+    
+?>
+
+<!DOCTYPE html>
+<html lang="fr">
+    <head>
+        <meta charset="utf-8"/>
+        <title>WestCoast</title>
+        <meta name="author" content="Florent NIGET Groupe17"/>
+        <link href="https://fonts.googleapis.com/css?family=Yatra+One&display=swap" rel="stylesheet">
+        <meta http-equiv="Content-Script-Type" content = "application /javascript" />
+        <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
+        <script type="application/javascript" src="js/script.js"></script>
+        <link href="Css/style.css" rel="stylesheet" type="text/css"/>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    </head>
+
+    <body>
+                 <!-- Affichage des billets -->
+                <section id="section_billets">
+                    <span id="billets">
+                    </span>
+                </section>
+    </body>
+</html>
