c2fc108481bdc9847c5ddd998842d234a255a569
Hector GODZIEN <hgodzien@gmail.com>
2020-12-04 00:37:15 +0100

    Root component working (just have to fix dimension)

--- SI-HexClan/projet/frontend/src/components/Root/main.js
+++ SI-HexClan/projet/frontend/src/components/Root/main.js
@@ -3,13 +3,25 @@ export default {
     name: 'Root',
     data: function() {
         return {
+            themes: [
+                {
+                    name: "Light",
+                    value: "light"
+                },
+                {
+                    name: "Dark",
+                    value: "dark"
+                }
+            ]
         }
     },
     computed: {
 
     },
     methods: {
-
+        updateTheme(value) {
+            this.$store.commit('updateTheme', value)
+        }
     },
     mounted:() => {
         
