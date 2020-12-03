5e05c240429d207626e08e80cb904957b09cd176
JeremyTo95 <jeremy.tourari@gmail.com>
2020-12-03 16:24:56 +0100

    dev API REST + E1-DESIGN.md

---
+++ si3-DDistrib/projet/E1-DESIGN.md
@@ -0,0 +1,16 @@
+# si3-DDistrib - Design
+
+## Schema architecture
+<img href="./assets/schema_architecture.png" with="100px" />
+(Paragraphe explication)
+
+## Modèle de donnéepngs de stockage
+<img href="./assets/data_model." with="100px" />
+(Paragraphe explicatif)
+
+## Définition du stack technique
+- NodeJS : Server
+- Express : Interface API
+- Database : fichier JSON
+- Python : Interface Graphique client
+(Pourquoi ?)
--- si3-DDistrib/projet/server.js
+++ si3-DDistrib/projet/server.js
@@ -1,42 +1,71 @@
 const express = require('express')
-const app = express();
+const fs      = require('fs');
+const app     = express();
 
-var clients = require('./data/clients.json');
-var airfares = require('./data/airfares.json');
-var airports = require('./data/airports.json');
-var flights = require('./data/flights.json');
+var clients   = require('./data/clients.json');
+var airfares  = require('./data/airfares.json');
+var airports  = require('./data/airports.json');
+var flights   = require('./data/flights.json');
 
 app.use(express.json());
 
-app.get('/', (req, res) => {
-	const data = {
-		"clients":  (clients),
-		"airports": (airports),
-		"flights":  (flights),
-		"airfares": (airfares)	
-	}
-	res.status(200).json(data);
+app.get('/', (req, res) => { // OK
+	res.send("API REST Architecture Distribué");
 });
 
+app.get('/Flights', (req, res) => { // OK
+	res.status(200).json(flights);
+})
+
 app.get('/myFlights/:id', (req, res) => {
-	res.send('Here are my personnal flights');
+	const id = parseInt(req.params.id)
+	const myFlights = [];
+	airfares.forEach((item) => {
+		if (item.idClient == id) myFlights.push(item)
+	})
+
+	console.log(myFlights);
+	res.status(200).json(myFlights)
 })
 
-app.get('/Flights', (req, res) => {
-	res.send('Here are all flights');
-})
+app.post('/myFlights/:idFlight&:idClient', (req, res) => { // OK
+	var id = 1;
+	airfares.forEach((item) => {
+		if (item.id >= id) {
+			id = item.id+1;
+		} 
+	});
+	
+	var targetFlight = null;
+	var fromLabel = null;
+	var toLabel = null;
 
-app.post('/Flights/:idFlight&:idClient', (req, res) => {
-	res.send('Here is the new flight of a client');
-})
+	flights.forEach((item) => {
+		if (item.id == req.params.idFlight) {
+			targetFlight = item;
+		}
+	});
+	airports.forEach((item) => {
+		if (item.name == targetFlight.from) fromLabel = item.label;
+		else if (item.name == targetFlight.to) toLabel = item.label;
+	})
 
-app.put('/myFlights/:id', (res, req) => {
-	res.send("Here is the update flight of a client");
-})
+	var price = targetFlight.price;
+	var airfareLabel = fromLabel + "-" + toLabel + " " + price;
 
-app.delete('/myFlights/:id', (res, req) => {
-	res.send("Here is the delete flight");
-})
+	const data = {
+		id: id,
+		idFlight: req.params.idFlight,
+		idClient: req.params.idClient,
+		label: airfareLabel
+	}
+	airfares.push(data);
+	fs.writeFile('./data/airfares.json', JSON.stringify(airfares), 'utf8', (err) => {
+		if (err) console.log('Error writing file : ' + err);
+		else console.log("File is written successfully !");
+	});
+	res.status(200).json(airfares);
+});
 
 app.listen(8000, () => {
 	console.log('Example app listening on port 8000!')
