69e1e0cad0c703d8d008d2c38590bcbbe44d701b
Florent-Ngt <nflorent7@gmail.com>
2020-12-03 15:31:20 +0100

    update script

--- ia1-WestCoast/projet/code/Js/script.js
+++ ia1-WestCoast/projet/code/Js/script.js
@@ -3,6 +3,53 @@
 /*****************************************************************************/
 
 window.onload = function() {
+	$(document).ready(function() {
+		$(document).on('click','.bouton', function()
+		{
+   			$.ajax({
+			   	type: 'POST',
+			   	url:'Php/achat.php',
+			   	data:{list:liste_billets},
+			   	success: function(rep){
+			   		
+			        if(result.length > 0){
+			            $('#billets').show();
+			        }
+			        else {$('#billets').empty(); $('#billet').hide();}
+			    },
+			    error:function(err){
+			        alert("erreur ! "+JSON.stringify(err)) ;
+			        $('#billets').hide();
+			    },
+			    complete:function(){
+			        ///alert("Terminé! ") ;
+			    }
+			});
+		});
+		$(document).on('click','#panier', function()
+		{
+			var client = new Array();
+			client[0] = "valentin";
+			client[1] = [0,1,2];
+			console.log(client);
+			$.ajax({
+				type: 'POST',
+				url: 'Php/panier.php',
+				data:{liste_billets: liste_billets},
+				success: function(rep){
+					result = JSON.parse(rep);
+					console.log(result);
+				},
+			    error:function(err){
+			        alert("erreur ! "+JSON.stringify(err)) ;
+			        $('#billets').hide();
+			    },
+			    complete:function(){
+			        ///alert("Terminé! ") ;
+			    }
+			});
+		});
+	});
 
 	var liste_billets;
 	/******************** Chargement de la liste de billets **************************/
@@ -17,33 +64,13 @@ window.onload = function() {
 	        	
 	        $.each(result, function(i, val) {
 	        	console.log(rep);
-		        $('#billets').append("<div id='"+i+"' class='billet'>"+val['depart']+ " -> " + val['arrive'] + " " + val['prix'] + "</div>");
+		        $('#billets').append("<div id='"+i+"' class='billet'>"+val['depart']+ " -> " + val['arrive'] + " " + val['prix'] + "<span class='bouton' id='"+i+"'> Acheter</span></div>");
 		    });
 	        if(result.length > 0){
 	            $('#billets').show();
 	        }
 	        else {$('#billets').empty(); $('#billet').hide();}
 	    },
-	    error:function(err){
-	        alert("erreur ! "+JSON.stringify(err)) ;
-	        $('#billets').hide();
-	    },
-	    complete:function(){
-	        ///alert("Terminé! ") ;
-	    }
-	});
-	var client = new Array();
-	client[0] = "valentin";
-	client[1] = [0,1,2];
-	console.log(client);
-	$.ajax({
-		type: 'POST',
-		url: 'Php/panier.php',
-		data:{liste_billets: liste_billets},
-		success: function(rep){
-			result = JSON.parse(rep);
-			console.log(result);
-		},
 	    error:function(err){
 	        alert("erreur ! "+JSON.stringify(err)) ;
 	        $('#billets').hide();
