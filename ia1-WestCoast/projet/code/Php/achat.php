cf5a1ca41739e0408bf62d4676d317f4ffe954e1
Timothe-Jutel <65890762+Timothe-Jutel@users.noreply.github.com>
2020-12-03 15:37:31 +0100

    update service achat

--- ia1-WestCoast/projet/code/Php/achat.php
+++ ia1-WestCoast/projet/code/Php/achat.php
@@ -1,11 +1,5 @@
 <?php
 include 'classe.php';
-function acheter_billet($client, $billet){
-	if (!in_array($billet->get_indice(), $client->get_liste()))
-	{
-    	$client->add_billet($billet->get_indice());
-	}
-}
 
 function acheter_indice($client, $billet){
 	if (!in_array($billet, $client->get_liste()))
@@ -14,15 +8,15 @@ function acheter_indice($client, $billet){
 	}
 }
 
-$liste_billets = creer_billets($aeroports);
-//var_dump($listes_billets);
-$client = new Client("florent");
-acheter_billet($client, $liste_billets[2]);
-acheter_billet($client, $liste_billets[3]);
-acheter_billet($client, $liste_billets[3]);
-echo json_encode($liste_billets);
-//echo json_encode($client);
-//var_dump($client);
-//echo json_encode(billets_achetes($client, $liste_billets));
-//var_dump(billets_achetes($client, $liste_billets));
+$tab = $_POST["client"];
+$client = new Client($tab[0]);
+
+foreach ($tab[1] as $b) {
+    $client->add_billet($b);
+}
+
+$billet = $_POST["nouveau"];
+
+acheter_indice($client, $billet);
+echo json_encode($client);
 ?>
