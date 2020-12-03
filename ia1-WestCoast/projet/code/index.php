6fb19dd1e6474e2d07fc13e5fb25151504275db8
Thomas RAIMBAULT <raimbault@et.esiea.fr>
2020-12-03 16:13:00 +0100

    Update interface client

--- ia1-WestCoast/projet/code/index.php
+++ ia1-WestCoast/projet/code/index.php
@@ -2,16 +2,15 @@
     //======================================================================
     // INDEX.PHP       TP - Prog distribuee       GROUPE - WestCoast
     //======================================================================
-    
 ?>
+
+
 
 <!DOCTYPE html>
 <html lang="fr">
     <head>
         <meta charset="utf-8"/>
         <title>WestCoast</title>
-        <meta name="author" content="Florent NIGET Groupe17"/>
-        <link href="https://fonts.googleapis.com/css?family=Yatra+One&display=swap" rel="stylesheet">
         <meta http-equiv="Content-Script-Type" content = "application /javascript" />
         <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.5.1/jquery.min.js"></script>
         <script type="application/javascript" src="js/script.js"></script>
@@ -19,11 +18,17 @@
         <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     </head>
 
-    <body>
-                 <!-- Affichage des billets -->
-                <section id="section_billets">
-                    <span id="billets">
-                    </span>
-                </section>
+    <body >        
+        <!-- Création du bloc d'en-tête: le logo et un slogan -->
+        <header class="wrapperR">
+            <h1 id="headerRight">West Coast</h1>
+            <form class="button_cont" align="center"><input type="submit" class="example_c" value="Panier" id="panier"></form>
+        </header>
+
+        <!-- Affichage des billets -->
+        <section id="section_billets">
+            <span id="billets">
+            </span>
+        </section>
     </body>
 </html>
