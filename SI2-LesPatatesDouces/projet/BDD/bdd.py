9882ac4007861eb5922fbfc300cef453f05c1524
lennaig <lennaig.charron@gmail.com>
2020-12-04 09:43:36 +0100

    id Aeroports ajoutes

--- SI2-LesPatatesDouces/projet/BDD/bdd.py
+++ SI2-LesPatatesDouces/projet/BDD/bdd.py
@@ -4,9 +4,9 @@ from model.aeroport import Aeroport
 class BDD:
     def __init__(self):
         self.list_aeroport = list()
-        self.list_aeroport.append(Aeroport("JFK", "New-York"))
-        self.list_aeroport.append(Aeroport("CDG", "Paris CDG"))
-        self.list_aeroport.append(Aeroport("DTW", "Detroit"))
+        self.list_aeroport.append(Aeroport(1,"JFK", "New-York"))
+        self.list_aeroport.append(Aeroport(2,"CDG", "Paris CDG"))
+        self.list_aeroport.append(Aeroport(3,"DTW", "Detroit"))
 
     def get_list_aeroport(self):
         return self.list_aeroport
