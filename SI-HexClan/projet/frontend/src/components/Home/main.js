08aedaee787efcc4c83f65911d8b04e8c1d55f56
ocruze <33525693+ocruze@users.noreply.github.com>
2020-12-07 12:37:33 +0100

    correctif bug book flight

--- SI-HexClan/projet/frontend/src/components/Home/main.js
+++ SI-HexClan/projet/frontend/src/components/Home/main.js
@@ -1,45 +1,46 @@
 /* eslint-disable no-console */
 export default {
-    name: 'Home',
+    name: "Home",
     data: function() {
         return {
-            flights: []
-        }
+            flights: [],
+        };
     },
-    computed: {
-
-    },
+    computed: {},
     methods: {
         updateTheme(value) {
-            this.$store.commit('updateTheme', value)
+            this.$store.commit("updateTheme", value);
         },
         bookFlight(value) {
-
             let config = {
                 headers: {
-                  'Authorization':  `Bearer ${this.$session.get('Token')}`,
-                  'Content-Type': 'application/json'
-                }
-              }
-            console.log(this.$session.get('Token'))
-            this.$axios.post("/flight-ticket/"+value, config).then((response) => {
-                console.log(response)
-                console.log("flight number"+ value +" is booked")
-            }).catch((error) => {
-                console.log(error)
-            })
-        }
+                    "Authorization": `Bearer ${this.$session.get("Token")}`,
+                    "Content-Type": "application/json",
+                },
+            };
+            console.log(this.$session.get("Token"));
+            this.$axios
+                .post("/flight-ticket/" + value, _, config)
+                .then((response) => {
+                    console.log(response);
+                    console.log("flight number" + value + " is booked");
+                })
+                .catch((error) => {
+                    console.log(error);
+                });
+        },
     },
     mounted: function() {
-        this.$axios.get("/flight-ticket/").then((response)=>{
-            // console.log(response)
-            this.flights = response.data
-            console.log(this.flights)
-        }).catch((error) => {
-            console.log(error)
-        })
+        this.$axios
+            .get("/flight-ticket/")
+            .then((response) => {
+                // console.log(response)
+                this.flights = response.data;
+                console.log(this.flights);
+            })
+            .catch((error) => {
+                console.log(error);
+            });
     },
-    components: {
-        
-    }
-}
+    components: {},
+};
