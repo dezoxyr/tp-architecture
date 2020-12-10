986c13c579c7666e873fee370df794a107b45457
AmZa <32337304+Amza99@users.noreply.github.com>
2020-12-10 12:42:55 +0100

    100% Last Update

--- IA-MaghrebUnited/Projet/Code/API.py
+++ IA-MaghrebUnited/Projet/Code/API.py
@@ -7,37 +7,122 @@ Created on Wed Dec  2 20:48:58 2020
 
 from flask import Flask
 from flask_restful import Resource, Api, reqparse
-import DB_Connector as DB 
+from DB_Connector import DATABASE as DB 
 """
 http://localhost:5000/billets
+http://localhost:5000/billets?Id_User=1
 http://localhost:5000/users
-http://127.0.0.1:5000/users?Nom=NOM_TEST&Prenom=Prenom_TEST
+http://localhost:5000/info
+http://127.0.0.1:5000/users?Nom=Boukhriss&Prenom=Hamza
+http://127.0.0.1:5000/billets?Nom=Bouyahy&Prenom=oumaima&Id_Vol=4
+http://127.0.0.1:5000/admin?Id_User=1
+
 """
-
+    
+    
 class Billet(Resource):
-    def get(self):
-        Connection = DB.DATABASE()
-        data = Connection.Get_Billet_Dispo()
-        data = data.to_dict()
+    def get(self):#get billet dispo
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('Id_User', required=True)
+        Connection = DB()
+        try :
+            args = parser.parse_args()  # parse arguments to dictionary
+            data = Connection.Get_Billet_Dispo_for_User(args["Id_User"])
+            try:
+                if( data == -1 ):
+                    return {'Status': "Tous est Reservé"}, 404
+            except :
+                pass
+            data = data.to_dict()
+            return {'data': data}, 200
+        except:
+            data = Connection.Get_Billet_Dispo()
+            data = data.to_dict()
+            Connection.Close()
+            return {'data': data}, 200
+    def post(self):#Reservation
+        parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('Mode', required=True)
+        parser.add_argument('Id_User', required=True)
+        parser.add_argument('Id_Vol', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+        Connection = DB()
+        if( args['Mode'] == "Delete" ):
+            Connection.Delete_Reservation(args['Id_User'],args['Id_Vol'])
+        if( args['Mode'] == "Reserve"):
+            if( Connection.Reservation(args['Id_User'],args['Id_Vol']) == -1 ):
+                Connection.Close()
+                return {'Status': "Deja reserver"}, 200
         Connection.Close()
-        return {'data': data}, 200
+        return {'Status': "Done"}, 200
 
-class Users(Resource):
-    def get(self):
-        Connection = DB.DATABASE()
-        data = Connection.Get_All_Users()
+class Info(Resource):
+    def get(self):# get all users reservation 
+        Connection = DB()
+        data = Connection.Get_ALL_Users_Reservation()
         data = data.to_dict()
         Connection.Close()
         return {'data': data}, 200
     def post(self):
         parser = reqparse.RequestParser()  # initialize
+        parser.add_argument('Id_User', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+        Connection = DB()
+        data =  Connection.Get_Reservation_User(args['Id_User'] )
+        Connection.Close()
+        try:
+            data += 1
+            return {'Status': "Aucune Reservatio n'est trouver "}, 401
+        except:
+            data = data.to_dict()
+            return {'data': data}, 200
+        
+class Users(Resource):
+    def get(self):# get list users
+        Connection = DB()
+        data = Connection.Get_All_Users()
+        data = data.to_dict()
+        Connection.Close()
+        return {'data': data}, 200
+    def post(self):# create a user
+        parser = reqparse.RequestParser()  # initialize
         parser.add_argument('Nom', required=True)
         parser.add_argument('Prenom', required=True)
         args = parser.parse_args()  # parse arguments to dictionary
-        Connection = DB.DATABASE()
-        Connection.Insert_New_User(args['Nom'] ,args['Prenom'] )
+        Connection = DB()
+        INDX = Connection.Return_INDX_User_Creat_user_if_not_existe(args['Nom'] ,args['Prenom'] )
         Connection.Close()
-        return {'Statut':'Done'},200
+        return {'Index': INDX },200
+    
+class Admin(Resource):
+    def get(self):
+        parser = reqparse.RequestParser()  # initialize
+        Connection = DB()
+        parser.add_argument('Mode', required=True)
+        parser.add_argument('Id', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+        if args['Mode'] == "Vol" :
+            Connection.Delete_Vol(args['Id'])
+        elif args['Mode'] == "User" :
+            Connection.Delete_User(args['Id'])
+        else:
+            return {'Status': 'Error' },404
+        return {'Status': 'Done' },200
+
+    def post(self):
+        parser = reqparse.RequestParser()  # initialize
+        Connection = DB()
+        parser.add_argument('From', required=True)
+        parser.add_argument('To', required=True)
+        parser.add_argument('Price', required=True)
+        parser.add_argument('Date', required=True)
+        parser.add_argument('NB', required=True)
+        args = parser.parse_args()  # parse arguments to dictionary
+        print( args['From'] , args['To'] , args['Price'] , args['Date'] , args['NB'] )
+        Connection.Create_Vol( args['From'] , args['To'] , args['Price'] , args['Date'] , args['NB'] )
+        Connection.Close()
+        return {'Status': 'Done' },200
+        
 
 
 app = Flask(__name__)
@@ -45,7 +130,8 @@ api = Api(app)
 
 api.add_resource(Users, '/users')  
 api.add_resource(Billet, '/billets') 
-
+api.add_resource(Info, '/info')  
+api.add_resource(Admin, '/admin')  
 
 
 app.run()
---
+++ IA-MaghrebUnited/Projet/E1-DESIGN.md
@@ -0,0 +1,15 @@
+# Design SYSTEM API
+
+![alt text](https://raw.githubusercontent.com/Amza99/tp-architecture/main/IA-MaghrebUnited/Projet/communicationSYS.png)
+
+# Technologies utilisée TP Architecture Distribué
+
+__Flask libraire python__ : un micro Framework open-source de création d’API
+
+__SQL lite__ : est une bibliothèque écrite en langage C qui propose un moteur de base de données relationnelle accessible par le langage SQL .
+
+__Web Scraping__ : Alimentation de notre base de donnée grâce à la collecte de donnée réel de réservation de base de donnée (Partie Bonus )
+
+__Web Programing__ : Mettre en place une interface web pour faciliter la réservation de billet grâce à notre API
+
+![alt text](https://raw.githubusercontent.com/Amza99/tp-architecture/main/IA-MaghrebUnited/Projet/modeleRelation.png)
