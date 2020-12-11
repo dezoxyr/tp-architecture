b599b6c9a8b117bdd65a49d9182e1004c53aba6a
maxime <maxime.abensur@gmail.com>
2020-12-11 02:38:02 +0100

    hard clean

--- SI-HexClan/projet/frontend/src/router/index.js
+++ SI-HexClan/projet/frontend/src/router/index.js
@@ -1,7 +1,5 @@
 import Vue from "vue";
 import VueRouter from "vue-router";
-//import { component } from 'vue/types/umd'
-//import Home from '../views/Home.vue'
 import Home from "../components/Home/component.vue";
 import Login from "../components/Login/component.vue";
 import Register from "../components/Register/component.vue";
