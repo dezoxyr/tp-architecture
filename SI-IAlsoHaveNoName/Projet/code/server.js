eebe482446b7919ba6a0dd2f0d13225eaa7d5c94
fanny <fanny@morvan.me>
2020-12-14 00:20:20 +0100

    fix bug

--- SI-IAlsoHaveNoName/Projet/code/server.js
+++
@@ -1,8 +0,0 @@
-var express = require('express');
-var http = require('http').createServer(app);
-var app = express();
-
-var server = app.listen(process.env.PORT || 2800,()=>
-{
-  console.log("I am running at PORT 2800")
-})
